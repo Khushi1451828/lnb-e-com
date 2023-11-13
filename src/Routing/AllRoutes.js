39984bd18bd5f6097400f4f193048689d0d66bff
ks1451828 <ks1451828@gmail.com>
2023-11-13 20:23:06 +0530

    usecontext hook

--- src/Routing/AllRoutes.js
+++ src/Routing/AllRoutes.js
@@ -9,6 +9,7 @@ import Navbar from '../Layout/Navbar'
 import ViewMore from '../Screens/ViewMore'
 import MemoDemo from '../Screens/UseMemoDemo'
 import UseCallbackDemo from '../Screens/UseCallbackDemo'
+import ContextDemo from '../Screens/ContextDemo'
 
 function AllRoutes(){
 
@@ -22,6 +23,7 @@ return(
     <Route path="/home"  element={<Home/>} />
     <Route path="/memodemo"  element={<MemoDemo/>} />
     <Route path="/callback-demo" element={<UseCallbackDemo/>}/>
+<Route path="/context-demo" element={<ContextDemo/>}/>
     <Route path="/about"  element={<About/>} />
     <Route path="/viewmore/:id" element={<ViewMore/>}/></Routes>
 </>
